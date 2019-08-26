14dc374208c662f8fefcc89eaea1bec1aed5cba4
Ralf Vogler <ralf.vogler@gmail.com>
2019-08-26 11:24:08 +0200

    script to rsync home to MBP before migration rpi3 to rpi4

---
+++ rpi-mbp-sync.sh
@@ -0,0 +1,9 @@
+# run to sync home to MBP
+# https://unix.stackexchange.com/questions/61586/how-to-tell-rsync-to-preserve-time-stamp-on-files-when-source-tree-has-a-mounted
+# -a implies -t for times, macOS rsync also has option -N/--crtimes to 'preserve create times (newnewss)' but that leads to an error of unknown option when rsyncing from rpi b/c its rsync doesn't have that option (despite being the same version)
+# -a archive mode; equals -rlptgoD (no -H,-A,-X)
+# -v verbose
+# -z compress file data during transfer
+# -P --partial (keep partially transferred files) --progress (show progress)
+# with -a `stat dash.log` shows 'Change' (ctime of inode?) as time of copy, but also has a field 'Birth' which shows the original create time
+rsync -avzP pi@rpi3.local:~/ rpi/
