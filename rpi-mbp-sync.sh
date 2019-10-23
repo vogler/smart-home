2247bf894a8dc9b98fac1439e3f1857c32de3a8e
Ralf Vogler <ralf.vogler@gmail.com>
2019-10-23 23:38:38 +0200

    truly sync (i.e. also delete), support dirs that need sudo, stats

--- rpi-mbp-sync.sh
+++ rpi-mbp-sync.sh
@@ -1,15 +1,23 @@
-# run to sync home to MBP
-# https://unix.stackexchange.com/questions/61586/how-to-tell-rsync-to-preserve-time-stamp-on-files-when-source-tree-has-a-mounted
-# -a implies -t for times, macOS rsync also has option -N/--crtimes to 'preserve create times (newnewss)' but that leads to an error of unknown option when rsyncing from rpi b/c its rsync doesn't have that option (despite being the same version)
+# rsync directories from rpi
+
+# notes on preserving creation time:
+  # https://unix.stackexchange.com/questions/61586/how-to-tell-rsync-to-preserve-time-stamp-on-files-when-source-tree-has-a-mounted
+  # -a implies -t for times, macOS rsync also has option -N/--crtimes to 'preserve create times (newnewss)' but that leads to an error of unknown option when rsyncing from rpi b/c its rsync doesn't have that option (despite being the same version)
+  # with `rsync -a`, `stat dash.log` shows 'Change' (ctime of inode?) as time of copy, but also has a field 'Birth' which shows the original create time
+  # https://unix.stackexchange.com/questions/2464/timestamp-modification-time-and-created-time-of-a-file
+
+# --delete truly sync target instead of just adding files
 # -a archive mode; equals -rlptgoD (no -H,-A,-X)
-# skip -H (--hard-links) because it's expensive, can use fdupes to hard-link duplicates, see https://news.ycombinator.com/item?id=8305545
-# skip -X (extended attributes, see https://en.wikipedia.org/wiki/Chattr#Attributes)
+  # skip -H (--hard-links) because it's expensive, can use fdupes to hard-link duplicates, see https://news.ycombinator.com/item?id=8305545
+  # skip -X (extended attributes, see https://en.wikipedia.org/wiki/Chattr#Attributes)
 # -v verbose
+# -h output numbers in a human-readable format
 # -z compress file data during transfer
 # -P --partial (keep partially transferred files) --progress (show progress)
-# with -a `stat dash.log` shows 'Change' (ctime of inode?) as time of copy, but also has a field 'Birth' which shows the original create time
-# https://unix.stackexchange.com/questions/2464/timestamp-modification-time-and-created-time-of-a-file
+# --stats file-transfer stats
+# --rsync-path need sudo for some directories and don't want to login as root
+
 dir=${1-"home/pi"}
 target=rpi/$dir/
 mkdir -p $target
-rsync -avzP pi@rpi3.local:/$dir/ $target
+rsync --delete -avhzP --stats --rsync-path="sudo rsync" pi@rpi3.local:/$dir/ $target
