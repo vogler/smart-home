55759e5ccd9df0912fce9947e42947d2237999de
Ralf Vogler <ralf.vogler@gmail.com>
2019-10-23 22:21:35 +0200

    rpi-mbp-sync.sh: arbitrary directory, default /home/pi/

--- rpi-mbp-sync.sh
+++ rpi-mbp-sync.sh
@@ -7,4 +7,7 @@
 # -P --partial (keep partially transferred files) --progress (show progress)
 # with -a `stat dash.log` shows 'Change' (ctime of inode?) as time of copy, but also has a field 'Birth' which shows the original create time
 # https://unix.stackexchange.com/questions/2464/timestamp-modification-time-and-created-time-of-a-file
-rsync -avzP pi@rpi3.local:~/ rpi/
+dir=${1-"home/pi"}
+target=rpi/$dir/
+mkdir -p $target
+rsync -avzP pi@rpi3.local:/$dir/ $target
