ffb02e8f1737786e8eb71c5a787d46f741edfd67
Ralf Vogler <ralf.vogler@gmail.com>
2021-11-02 22:54:15 +0100

    merge switchbot-curtain.py into switchbot-cmd.py and clean up

--- switchbot-cmd.py
+++ switchbot-cmd.py
@@ -1,34 +1,47 @@
 #!/usr/bin/env python3
-# Simplest Switchbot Command
-# https://gist.github.com/mugifly/a29f34df7de8960d72245fcb124513c7
-# Thanks to https://gist.github.com/aerialist/163a5794e95ccd28dc023161324009ed
+# Send commands to SwitchBot Bot and Curtain.
+# Extended https://gist.github.com/mugifly/a29f34df7de8960d72245fcb124513c7
 
 import sys
 import binascii
 from bluepy.btle import Peripheral
 
 if len(sys.argv) != 3:
-    print('[Usage] python %s ff:ff:ff:ff:ff:ff MODE' % sys.argv[0])
-    print('\nMODE: on, off, press')
-    print('\nYou can get a MAC address of Switchbot, with using $ sudo hcitool lescan')
+    print('[Usage] python %s ff:ff:ff:ff:ff:ff CMD' % sys.argv[0])
+    print('CMD:')
+    print('  bot:     on, off, press, hold (5s)')
+    print('  curtain: open, close')
+    print('\nYou can get the MAC address of a Switchbot using `sudo hcitool lescan`')
     quit(1)
 
-mac = sys.argv[1]
-mode = sys.argv[2]
+mac = sys.argv[1].lower()
+cmd = sys.argv[2].lower()
 
-print('Connecting... ' + mac)
+# bot
+# https://github.com/OpenWonderLabs/python-host/wiki/Bot-BLE-open-API#0x01-execute-an-action
+if cmd == 'on':
+    s = '570101'
+elif cmd == 'off':
+    s = '570102'
+elif cmd == 'press':
+    s = '570100'
+elif cmd == 'hold':
+    s = '5701030504' # byte 05 is pause in seconds
+
+# curtain
+# https://github.com/OpenWonderLabs/python-host/wiki/Curtain-BLE-open-API#0x45-curtain-setting-command
+elif cmd == 'open':
+    s = '570f450105ff00'
+elif cmd == 'close':
+    s = '570f450105ff64'
+else:
+    print('Unknown command', cmd)
+    quit(1)
+
+print('Connecting to', mac)
 p = Peripheral(mac, 'random')
-hand_service = p.getServiceByUUID('cba20d00-224d-11e6-9fb8-0002a5d5c51b')
-hand = hand_service.getCharacteristics('cba20002-224d-11e6-9fb8-0002a5d5c51b')[0]
-
-if mode == 'on':
-    print('On')
-    hand.write(binascii.a2b_hex('570101'))
-elif mode == 'off':
-    print('Off')
-    hand.write(binascii.a2b_hex('570102'))
-elif mode == 'press':
-    print('Press')
-    hand.write(binascii.a2b_hex('570100'))
-
+service = p.getServiceByUUID('cba20d00-224d-11e6-9fb8-0002a5d5c51b')
+characteristic = service.getCharacteristics('cba20002-224d-11e6-9fb8-0002a5d5c51b')[0]
+characteristic.write(binascii.a2b_hex(s))
 p.disconnect()
+print('Executed', cmd)
